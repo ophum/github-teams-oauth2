4b24bc20e9d0871ffd4f099e361a5e3073fa34dc
ophum <inagaki0106@gmail.com>
2024-11-11 22:49:15 +0900

    ファイル分割

--- internal/server/server.go
+++ internal/server/server.go
@@ -22,7 +22,6 @@ import (
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"github.com/ophum/github-teams-oauth2/ent"
-	"github.com/ophum/github-teams-oauth2/ent/accesstoken"
 	"github.com/ophum/github-teams-oauth2/ent/code"
 	"github.com/ophum/github-teams-oauth2/ent/group"
 	"github.com/ophum/github-teams-oauth2/internal/config"
@@ -415,43 +414,6 @@ func (s *Server) postOauth2TokenHandle(ctx echo.Context) error {
 		if err != nil {
 			return err
 		}
-	}
-	return ctx.JSON(http.StatusOK, ret)
-}
-
-func (s *Server) getUserinfoHandle(ctx echo.Context) error {
-	token, err := getBearerToken(ctx.Request().Header)
-	if err != nil {
-		return echo.ErrUnauthorized
-	}
-
-	hashedToken := sha512String(token)
-	accessToken, err := s.db.AccessToken.Query().
-		Where(accesstoken.Token(hashedToken)).
-		First(ctx.Request().Context())
-	if err != nil {
-		return err
-	}
-
-	user, err := accessToken.QueryUser().First(ctx.Request().Context())
-	if err != nil {
-		return err
-	}
-
-	groups, err := accessToken.QueryGroups().All(ctx.Request().Context())
-	if err != nil {
-		return err
-	}
-
-	ret := map[string]any{
-		"id":       user.ID.String(),
-		"username": user.Name,
-		"email":    user.Email,
-	}
-	if len(groups) > 0 {
-		ret["groups"] = slicesMap(groups, func(v *ent.Group) string {
-			return v.Name
-		})
 	}
 	return ctx.JSON(http.StatusOK, ret)
 }
